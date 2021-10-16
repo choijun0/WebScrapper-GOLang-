0f6845c057ee95f174543560c31fc5da5f9f536c
Choijun0 <73348238+choijun0@users.noreply.github.com>
2021-10-16 09:13:11 +0000

    <Error version>

--- main.go
+++ main.go
@@ -11,18 +11,17 @@ import (
 var baseURL = "https://kr.indeed.com/jobs?q=javascript";
 
 func main() {
-  //pageCount := getPagesCount();
-getPage(0)
+  itemCount := getPagesItemCount();
 
-  /*
-  for i:=0; i<pageCount; i++ {
-    getPage(i)
+  for i:=0; ; i++ {
+    if getPage(i, itemCount) != false {
+      break;
+    }
   }
-  */
 }
 
 //form the url of each page extracted by getPages func and by using them exrtact extract more info
-func getPage(page int) {
+func getPage(page int, itemCount int) bool {
   pageURL := "";
   if page != 0{
     pageURL = baseURL + "&start=" + strconv.Itoa(page * 10);
@@ -32,10 +31,11 @@ func getPage(page int) {
   fmt.Println("Requesting ", pageURL);
   res, err := http.Get(pageURL);
   defer res.Body.Close()
-  checkErr(err);
+  checkErr(err)
   checkStatus(res);
   doc, _ := goquery.NewDocumentFromReader(res.Body);
   cards := doc.Find(".tapItem");
+  fmt.Println(cards.Length())
   cards.Each(func(i int, card *goquery.Selection){
     id, _ := card.Attr("id");
     infoCon := card.Find(".resultContent");
@@ -48,22 +48,25 @@ func getPage(page int) {
     
     fmt.Println(id, title, company, location);
   })
-
+    //check is LastPage
+  if cards.Length() != itemCount {
+    return true;
+  }
+  return false;
 }
 
 //Get html file of baseURL(By goquery) and Exract page count
-func getPagesCount() int{
-  pages := 0;
+func getPagesItemCount() int{
   res, err := http.Get(baseURL);
   defer res.Body.Close()
   checkErr(err);
   checkStatus(res);
   doc, err := goquery.NewDocumentFromReader(res.Body)
   checkErr(err);
-  doc.Find(".pagination").Each(func(i int, s *goquery.Selection){
-    pages = s.Find("a").Length();
-  }) 
-  return pages;
+  //get num of selections
+  itemCount := doc.Find(".tapItem").Length();
+
+  return itemCount;
 }
 
 func checkErr(err error){
