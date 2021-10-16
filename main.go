3eabca337d2d76e9b605a9e4063aa986e211bcf5
Choijun0 <73348238+choijun0@users.noreply.github.com>
2021-10-16 10:59:46 +0000

    <Extract Data and write csv file>

--- main.go
+++ main.go
@@ -7,22 +7,54 @@ import (
   "log"
   "strconv"
   "strings"
+  "os"
+  "encoding/csv"
 )
 
 var baseURL = "https://kr.indeed.com/jobs?q=javascript";
 
-func main() {
-  pages := getPagesItemCount();
+type extractedInfo struct {
+  id string
+  title string
+  company string 
+  location string
+  salary string
+  summary string 
+} 
 
-  for i:=0; i<pages; i++ {
+func main() {
+  //pages := getPages();
+  var jobsInfo []extractedInfo
+  for i:=0; i<5; i++ {
     //exrtact func(http.Get) is usynchronized func so it and for func can't be come together
-    getPage(i)
+    jobsInfo=append(jobsInfo, getPage(i)...); //the way spread array in go
+  }
+  writeJobs(jobsInfo);
+}
+
+func writeJobs(jobsInfo []extractedInfo) {
+  file, err := os.Create("jobs.csv");
+  checkErr(err);
+
+  w:= csv.NewWriter(file);
+  defer w.Flush()
+
+  headers := []string{"URL", "TITLE", "COMPANY", "LOCATION", "SALARY", "SUMMARY"}
+  w.Write(headers);
+
+  for _, job := range jobsInfo {
+
+    jobSlice := []string{"https://kr.indeed.com/jobs?q=javascript&vjk=" + job.id[4 : len(job.id) -1], job.title,job.company, job.location, job.salary, job.summary}
+    err := w.Write(jobSlice)
+    checkErr(err)
   }
 }
 
+
 //form the url of each page extracted by getPages func and by using them exrtact extract more info
-func getPage(page int) {
+func getPage(page int) []extractedInfo {
   pageURL := "";
+  var jobInfoCon []extractedInfo;
   if page != 0{
     pageURL = baseURL + "&start=" + strconv.Itoa(page * 10);
   } else {
@@ -34,24 +66,51 @@ func getPage(page int) {
   checkErr(err)
   checkStatus(res);
   doc, _ := goquery.NewDocumentFromReader(res.Body);
-  cards := doc.Find(".tapItem");
-  fmt.Println(cards.Length())
+  cards := doc.Find(".tapItem")
   cards.Each(func(i int, card *goquery.Selection){
-    id, _ := card.Attr("id");
-    infoCon := card.Find(".resultContent");
-
-    title, _ := infoCon.Find(".jobTitle>span").Attr("title");
-    
-    company_location := infoCon.Find(".company_location>pre");
-    company := company_location.Find(".companyName>a").Text()
-		location := company_location.Find(".companyLocation").Text()
-    
-    fmt.Println(id, title, company, location);
+    jobInfo := extractJonInfo(card);
+    jobInfoCon = append(jobInfoCon, jobInfo);
   })
+
+  return jobInfoCon;
 }
 
+
+func extractJonInfo(card *goquery.Selection) extractedInfo{
+    id, _ := card.Attr("id");
+
+    title, _ := card.Find(".jobTitle>span").Attr("title");
+    title = cleaningString(title);
+    
+    company_location := card.Find(".company_location>pre");
+    company := cleaningString(company_location.Find(".companyName>a").Text())
+		location := cleaningString(company_location.Find(".companyLocation").Text())
+
+    //can't extract!!!
+    summary := cleaningString(card.Find(".snippet").Text())
+    
+    salary := cleaningString(card.Find(".salary-snippet-container>span").Text())
+
+    return extractedInfo{
+      id: id, 
+      title: title,
+      company: company, 
+      location: location,
+      summary: summary, 
+      salary: salary}
+}
+
+func cleaningString(str string) string{
+  //#.1 Trimspace를 이용하여 문자열의 양옆 공백제거
+  //#.2 Fields 를 이용해 text만을 요소로하는 문자배열생성
+  //#.3 Join을 이용해 " "을 사이에두고 문자배열을 문자열로 합침
+  return strings.Join(strings.Fields(strings.TrimSpace(str))," ");
+}
+
+
+
 //Get html file of baseURL(By goquery) and Exract page count
-func getPagesItemCount() int{
+func getPages() int{
   res, err := http.Get(baseURL);
   defer res.Body.Close()
   checkErr(err);
